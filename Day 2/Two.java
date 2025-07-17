0e501d1bad339f79c19d8ab2f0a5442ef4dae915
therohit15 <rohitk15102004@gmail.com>
2025-07-17 23:44:10 +0530

    add day 2 q 2

---
+++ Day 2/Two.java
@@ -0,0 +1,10 @@
+// 71
+
+
+import java.util.*;
+class New{
+    public static void main(String[] args){
+        int a = new Scanner(System.in).nextInt();
+        System.out.println((char)a);
+    }
+}
