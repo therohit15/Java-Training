43963e4bc35b08c3900010e416123f18abeb6f5c
therohit15 <rohitk15102004@gmail.com>
2025-07-18 00:07:45 +0530

    Added day 2 q5

---
+++ Day 2/Five.java
@@ -0,0 +1,43 @@
+// Q4.Write a java program to get 2 numbers from the user and 
+// swap their values without any loss of data.
+// You cannot make use of additional variable for swapping.
+// Print the corresponding swapped values of the two numbers as output in the console.
+// Input format:
+// First input: an integer
+// Second input: an integer
+// Output format:
+// The output will be integers(swapped values)
+// Sample Input:
+// 20
+// 10
+// Sample Output:
+// 10
+// 20
+// Input (stdin)
+// 5
+// 10
+// Output (stdout)
+// 10
+// 5
+
+import java.util.*;
+public class Five{
+    public static void main(String[] args){
+	    Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    a = a + b;
+	    b = a - b;
+	    a = a - b;
+        System.out.printf("%d %d\n",a,b);
+        a = a * b;
+	    b = a / b;
+	    a = a / b;
+        System.out.printf("%d %d\n",a,b);
+        a = a ^ b;
+	    b = a ^ b;
+	    a = a ^ b;
+        System.out.printf("%d %d\n",a,b);
+        sc.close();
+    }
+}
